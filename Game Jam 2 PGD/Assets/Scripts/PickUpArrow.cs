1324bd6b5f88209b4e68d4e27e958589cad0f2c9
RazorShake <72439284+RazorShake@users.noreply.github.com>
2021-09-18 21:19:08 +0200

    Pickup arrow

---
+++ Game Jam 2 PGD/Assets/Scripts/PickUpArrow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpArrow : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PickUpManager manager = collision.GetComponent<PickUpManager>();
+        if(manager)
+        {
+            manager.PickUpItem();
+            Destroy(gameObject);
+        }
+      
+    }
+}
---
+++ Game Jam 2 PGD/Assets/Scripts/PickUpManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpManager : MonoBehaviour
+{
+    public int arrowCount;
+
+    public void PickUpItem()
+    {
+        arrowCount++;
+    }
+
+}
