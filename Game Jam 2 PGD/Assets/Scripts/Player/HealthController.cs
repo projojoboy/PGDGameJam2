b0b6b86d6b151a766ef4cb95badf763111ca853f
DESKTOP-OQPBCM1\Joey <joeykossen@hotmail.com>
2021-09-21 00:17:44 +0200

    Add Health System

---
+++ Game Jam 2 PGD/Assets/Scripts/Player/HealthController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 3;
+    [SerializeField] private int _currentHealth = 3;
+
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private GameObject _healthBar;
+    [SerializeField] private GameObject _heart;
+
+    private GameObject[] _hp;
+
+    private void Awake()
+    {
+        if (GameObject.FindObjectOfType<Canvas>())
+            _canvas = GameObject.FindObjectOfType<Canvas>();
+        else
+            _canvas = Instantiate(_canvas);
+
+        _healthBar = Instantiate(_healthBar, _canvas.transform);
+
+        _hp = new GameObject[3];
+
+        for (int i = 0; i < _maxHealth; i++)
+        {
+            GameObject heart = _hp[i] = Instantiate(_heart, _healthBar.transform);
+            heart.transform.position = new Vector2(_healthBar.transform.position.x + (60 * i), _healthBar.transform.position.y);
+        }
+    }
+
+    private void Update()
+    {
+        if (_currentHealth <= 0)
+            Dead();
+    }
+
+    private void Dead()
+    {
+        Debug.Log("F for sad me...");
+    }
+
+    public void LoseHP(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            if (_currentHealth > 0)
+            {
+                _currentHealth--;
+                _hp[_currentHealth].SetActive(false);
+            }
+        }
+    }
+
+    public void GainHP(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            if (_currentHealth < _maxHealth)
+            {
+                _hp[_currentHealth].SetActive(true);
+                _currentHealth++;
+            }
+        }
+    }
+}
